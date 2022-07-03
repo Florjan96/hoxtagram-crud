dec9beba6c56af1e2c4d8ae1b4c113d2922a737b
Florjan96 <florianshiqerukaj@yahoo.com>
2022-07-03 23:47:41 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,66 @@
 // solution goes here
+function getImage(){
+
+
+    let mondi=document.querySelector('image-container')
+//     <article class="image-card">
+//     <h2 class="title">Title of image goes here</h2>
+//     <img src="./assets/image-placeholder.jpg" class="image" />
+//     <div class="likes-section">
+//       <span class="likes">0 likes</span>
+//       <button class="like-button">♥</button>
+//     </div>
+//     <ul class="comments">
+//       <li>Get rid of these comments</li>
+//       <li>And replace them with the real ones</li>
+//       <li>From the server</li>
+//     </ul>
+//   </article>
+let article=document.createElement('article')
+article.className="image-card"
+
+let title=document.createElement('h2')
+title.className="title"
+title.textContent="Title of image goes here"
+
+let img=document.createElement('img')
+img.className="image"
+img.src="./assets/image-placeholder.jpg"
+
+let div=document.createElement('div')
+div.className="likes-section"
+
+let span=document.createElement('span')
+span.className="likes"
+span.textContent="0 likes"
+
+let button=document.createElement('button')
+button.className="like-button"
+button.textContent="♥"
+
+let ulComments=document.createElement('ul')
+ulComments.className="comments"
+
+let liFirst=document.createElement('li')
+liFirst.textContent="Get rid of these comments"
+
+let liSecond=document.createElement('li')
+liSecond.textContent="And replace them with the real ones"
+
+let liThird=document.createElement('li')
+liThird.textContent="From the server"
+
+
+div.append(span,button)
+ulComments.append(liFirst,liSecond,liThird)
+article.append(div,ulComments,title,img)
+
+console.log(article)
+
+
+
+
+
+}
+
+getImage()
